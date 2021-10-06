dde1b2041151eca26ac4ec90560edd56f1cc055d
davidwobido <davidwobido@googlemail.com>
2021-10-06 15:53:48 +0200

    Changed filenames

---
+++ src/components/Tabs/Food-recipe-tab.stories.js
@@ -0,0 +1,9 @@
+import "./Food-recipe-tab.css";
+import favoriteCard from "./Food-recipe-tab.html";
+
+export default {
+  title: "Components/Tabs",
+  parameters: { layout: "centered" },
+};
+
+export const FoodRecipes = () => favoriteCard;
